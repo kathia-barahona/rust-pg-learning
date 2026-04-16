879b10a732c985db716ff7c690cffc8c6b99b8d0
Kathia Barahona <katbara03@gmail.com>
2026-04-16 17:36:00 +0200

    basic tcp echo server
    Implemented TcpListener and TcpStream, the echo server binds to
    a default port (5000) and listens. When a client connects, it'll
    read from the stream and write back.

--- protocol-proxy/src/main.rs
+++ protocol-proxy/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+use std::io::{Write, Read};
+
+fn handle_client(mut stream: TcpStream) {
+    println!("Connected to {}", stream.peer_addr().unwrap());
+    let buffer_size: usize = 1024;
+    let mut buffer = vec![0u8; buffer_size];
+    loop {
+        // only write back what it was read
+        let n = stream.read(&mut buffer).unwrap();
+        if n == 0 {
+            println!("Client disconnected");
+            break;
+        }
+        stream.write_all(&buffer[..n]).unwrap();
+    }
+}
+
+fn listen_on(port: u16) -> TcpListener{
+    let listener = TcpListener::bind(("127.0.0.1", port)).unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+    listener
+}
+fn main() -> std::io::Result<()> {
+    let port: u16 = 5000;
+    // accept connections and process them serially
+    for stream in listen_on(port).incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
 }
